bf4d30a8a5af845162becee31fc5cf8f96ec38de
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-21 13:58:47 +0100

    Ajout de errorHandler

--- server.js
+++ server.js
@@ -3,6 +3,37 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 app.set('port', port);
-const server = http.createServer(app)
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const address = server.address();
+    const bind = typeof address === "string" ? 'pipe ' + address : 'port: ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit();
+            break;
+    
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    console.log('Listening on '+bind);
+})
 
 server.listen(port);
