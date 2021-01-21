0c2de5a17f689972486d2a7b6ac21fa58b0424ac
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-21 14:18:49 +0100

    Ajout de stuffs

--- app.js
+++ app.js
@@ -2,10 +2,35 @@ const expess = require('express');
 
 const app = expess();
 
-app.use((req, res) => {
-    res.json({
-        message: "Votre requête a bien été réçue !"
-    })
+// Ajout de CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api/stuff', (req, res) => {
+    const stuff = [
+        {
+            _id: '5djhd51d2s',
+            title: "Mon premier object",
+            description: "Les infos du premier objet",
+            imageUrl: 'https://cdn.pixabay.com/photo/2020/06/24/14/59/aircraft-5336532_960_720.jpg',
+            price: 4900,
+            userId: 'violet12345'
+        },
+        {
+            _id: '5djhd51d2s',
+            title: "Mon deuxieme object",
+            description: "Les infos du deuxieme objet",
+            imageUrl: 'https://cdn.pixabay.com/photo/2020/06/24/14/59/aircraft-5336532_960_720.jpg',
+            price: 2900,
+            userId: 'violet12345'
+        }
+    ];
+
+    res.status(200).json(stuff);
 })
 
 module.exports = app
