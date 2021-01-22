295ab38e1eb91c480c3f6761a54cad6ab7959647
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-22 17:34:19 +0100

    Route de updating thing

--- app.js
+++ app.js
@@ -48,4 +48,10 @@ app.get('/api/stuff/:id', (req, res) => {
         .catch(error => res.status(404).json({error}))
 })
 
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Objet modifié !" }))
+        .catch(error => res.status(400).json({error}))
+})
+
 module.exports = app
