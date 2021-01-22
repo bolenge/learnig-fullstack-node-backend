314962f796215d42c092eced371f0394cf84ef12
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-22 17:35:17 +0100

    Route de suppression

--- app.js
+++ app.js
@@ -54,4 +54,10 @@ app.put('/api/stuff/:id', (req, res, next) => {
         .catch(error => res.status(400).json({error}))
 })
 
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+        .catch(error => res.status(400).json({error}))
+})
+
 module.exports = app
