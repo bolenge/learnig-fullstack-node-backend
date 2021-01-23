a156323fa70d63cf9c2ac7531120b1f5c4be0d41
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-23 16:21:51 +0100

    Creation module de connexion utilisateur

--- controllers/user.js
+++ controllers/user.js
@@ -23,6 +23,31 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(500).json({ error }))
 };
 
+/**
+ * Module de connexion de l'utilisateur
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @param {Function} next 
+ */
 exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' })
+            }
 
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' })
+                    }
+
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    })
+                })
+                .catch(error => res.status(500).json({ error }))
+        })
+        .catch(error => res.status(500).json({ error }))
 };
