630b3a95adebd1dc88ef32e8146c0c5aef6a2169
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-01-23 20:42:43 +0100

    Ajout du multer dans le router

--- routes/stuff.js
+++ routes/stuff.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, stuffCtrl.createThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
 router.get('/', auth, stuffCtrl.getAllThing)
 router.get('/:id', auth, stuffCtrl.getOneThing)
 router.put('/:id', auth, stuffCtrl.modifyThing)
